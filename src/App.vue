39b9ca4ab5a61751b8c5080e7521dd61567f935d
khactam <khactam95@gmail.com>
2019-09-06 16:51:36 +0300

    finalizing frontend

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,6 @@
 <template>
   <div id="app">
-    <button v-for="area in areas" :key="area.id" @click="changeData(area)">{{area.name}}</button>
-    <Display :selectedArea="selectedArea" :spec="spec" />
+    <Display/>
   </div>
 </template>
 
@@ -13,39 +12,8 @@ export default {
   components: {
     Display
   },
-  data: () => ({
-    spec: null,
-    areas: [],
-    selectedArea: {}
-  }),
   async mounted () {
-    /* eslint-disable */
-    this.getArea().then((areas) => {
-      this.areas = areas.map((area) => {
-        return {
-          name: area.get('name'),
-          range: area.get('frequencies'),
-          id: area.get('id')
-        }
-      })
-    })
-  },
-  methods: {
-    getArea () {
-      return Parse.Cloud.run('getArea', {})
-    },
-    getSpec: async function (areaName) {
-      return Parse.Cloud.run('getSpec', { area: areaName }).then(response => {
-        return response;
-      }).catch(err => {
-        throw err
-      })
-    },
-    changeData: async function (area) {
-      this.selectedArea = area
-      this.spec = await this.getSpec(area.name)
-    }
-  },
+  }
 }
 </script>
 
--- src/components/Display.vue
+++ src/components/Display.vue
@@ -1,39 +1,20 @@
 <template>
   <div class="container">
-    <blood-data :chartData="chartdata" :options="options"></blood-data>
+    <button v-for="area in areas" :key="area.id" @click="changeData(area)">{{area.name}}</button>
+    <blood-data ref="chart" :chartData="chartdata" :options="options"></blood-data>
+    <div v-show="!loaded" class="backdrop" />
+    <circle8 v-show="!loaded" class="overlay-spinner"></circle8>
   </div>
 </template>
 <script>
 import BloodData from './BloodData'
+import { Circle8 } from 'vue-loading-spinner'
 export default {
-  components: { BloodData },
-  props: {
-    selectedArea: {},
-    spec: null
-  },
-  watch: {
-    selectedArea: {
-      handler: function (newValue) {
-        this.chartdata.datasets.label = newValue.range
-        this.chartdata.datasets[0].label = newValue.name
-      },
-      deep: true
-    },
-    spec: {
-      handler: function (newValue) {
-        this.chartdata.datasets[0].data = newValue
-        console.log(this.chartdata)
-      },
-      deep: true
-    }
-  },
+  components: { BloodData, Circle8 },
   data: () => ({
     loaded: false,
     chartdata: {},
-    options: null
-  }),
-  async mounted () {
-    this.options = {
+    options: {
       scales: {
         xAxes: [{
           type: 'linear',
@@ -42,33 +23,74 @@ export default {
       },
       responsive: true,
       maintainAspectRatio: false
-    }
+    },
+    areas: [],
+    spec: null
+  }),
+  async mounted () {
+    /* eslint-disable */
+    this.getArea().then((areas) => {
+      this.loaded = true
+      this.areas = areas.map((area) => {
+        return {
+          name: area.get('name'),
+          range: area.get('frequencies'),
+          id: area.get('id')
+        }
+      })
+    })
     /* eslint-disable */
     this.chartdata = {
       labels: [-0.6, 0.2],
       datasets: [
         {
-          label: 'Things',
-          data: [{ x: -0.5999923808, y: 2432.0826306537833 }, { x: -0.599763805, y: 2265.323385061954 }, { x: -0.5995352292, y: 2163.1145665703098 }, { x: -0.5990780777, y: 2129.988906045488 }, { x: -0.5988495019, y: 2140.93779104109 }
-            , { x: -0.5986209261, y: 2133.5425598753936 }
-            , { x: -0.5983923503, y: 2090.800070597172 }
-            , { x: -0.5981637745, y: 2012.0625780884388 }
-            , { x: -0.5979351988, y: 1919.7399740355147 }
-            , { x: -0.597706623, y: 1846.537290743382 }
-            , { x: -0.5974780472, y: 1810.7444314186312 }
-            , { x: -0.5972494714, y: 1806.0989308146713 }
-            , { x: -0.5970208956, y: 1814.2930511508116 }
-            , { x: -0.5967923199, y: 1824.0300422933472 }
-            , { x: -0.5965637441, y: 1835.3435160195309 }
-            , { x: -0.5963351683, y: 1842.6327904205255 }
-            , { x: -0.5961065925, y: 1819.6928520895342 }]
+          label: 'Demo',
+          borderWidth: 1,
+          pointRadius: 0,
+          data: [{ x: -0.5999923808, y: 2432.0826306537833 }, { x: -0.599763805, y: 2265.323385061954 }, { x: -0.5995352292, y: 2163.1145665703098 }]
         }
       ]
     }
   },
   methods: {
+    getArea () {
+      return Parse.Cloud.run('getArea', {})
+    },
+    getSpec: async function (areaName) {
+      return Parse.Cloud.run('getSpec', { area: areaName }).then(response => {
+        return response;
+      }).catch(err => {
+        throw err
+      })
+    },
+    changeData: async function (area) {
+      this.loaded = false
+      this.chartdata.datasets[0].data = await this.getSpec(area.name)
+      console.log(this.$refs.chart);
+      this.chartdata.labels = area.range
+      this.chartdata.datasets[0].label = area.name
+      this.$refs.chart.renderChart(this.chartdata, this.options)
+      setTimeout(() => {
+        this.loaded = true
+      }, 0);
+    }
   }
 }
 </script>
 <style scoped>
+.overlay-spinner {
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+}
+.backdrop {
+  position: absolute;
+  top: 0;
+  left: 0;
+  height: 100vh;
+  width: 100vw;
+  background: black;
+  opacity: 0.5;
+}
 </style>
